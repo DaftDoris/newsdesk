7532428e8a8d026e4da4b65b300b8e9893a82e60
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-04-22 15:48:17 +0100

    menu selects the podcast

--- src/components/organisms/AppBar.vue
+++ src/components/organisms/AppBar.vue
@@ -11,7 +11,7 @@
           <MenuButton
             class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
           >
-            Smart Seven 🇬🇧
+          {{this.$route.params.podcastId}}
             <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
           </MenuButton>
         </div>
@@ -30,7 +30,7 @@
             <div class="py-1">
               <MenuItem v-slot="{ active }">
                 <a
-                  href="#"
+                  href="/smartseven"
                   :class="[
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm',
@@ -40,7 +40,7 @@
               </MenuItem>
               <MenuItem v-slot="{ active }">
                 <a
-                  href="#"
+                  href="/smartseven-ireland"
                   :class="[
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm',
