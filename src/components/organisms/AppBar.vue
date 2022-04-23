f5f0467f173a98057968f5d1db57df4cb2b5fb5b
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-04-23 10:54:55 +0100

    fix menu

--- src/components/organisms/AppBar.vue
+++ src/components/organisms/AppBar.vue
@@ -11,10 +11,7 @@
           <MenuButton
             class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
           >
-            {{
-              //@ts-ignore
-              this.$route.params.podcastId || "SELECT PODCAST"
-            }}
+            {{ route.params.podcastId || "smartseven" }}
             <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
           </MenuButton>
         </div>
