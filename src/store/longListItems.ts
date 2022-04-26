647ae107705260a7306fc367db975df062314a65
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-04-26 20:39:07 +0100

    crude implementation of long list

---
+++ src/store/longListItems.ts
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia"
+import { Item } from "@/types/item"
+import { podcasts } from "@/store/podcasts"
+
+import { doc, getFirestore, onSnapshot } from "firebase/firestore"
+
+interface longListItem {
+  id: string
+  name: string
+  slotTitles: string[]
+  items: Item[]
+}
+
+interface State {
+  itemLongList: longListItem[]
+}
+
+export const uselongListItemsStore = defineStore("longListItems", {
+  state: (): State => ({
+    itemLongList: podcasts.map((podcast) => ({
+      id: podcast.id,
+      name: podcast.name,
+      slotTitles: [],
+      items: [],
+    })),
+  }),
+  actions: {
+    connect(docname: string) {
+      const db = getFirestore()
+      podcasts.forEach((podcast, i) =>
+        onSnapshot(doc(db, podcast.id, docname), (doc) => {
+          this.itemLongList[i] = {
+            slotTitles: (doc.data()?.slotTitles ?? []) as string[],
+            items: (doc.data()?.items ?? []) as Item[],
+            ...podcast,
+          }
+        }),
+      )
+    },
+  },
+  getters: {
+    getLongList: (state: State) => state.itemLongList,
+  },
+})
