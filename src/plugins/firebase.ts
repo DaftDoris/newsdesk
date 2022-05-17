46a8207b5ec297b01100e47faa1fe6d6ffcb41e2
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-05-17 19:38:54 +0100

    Cypress (#231)
    * interim
    
    * testing
    
    * cypress dump
    
    * lint
    
    * cypress config
    
    * lint
    
    * ci yaml
    
    * build first
    
    * bigger viewport
    
    * record test runs to cypress dashboard
    
    * fiddle ci
    
    * scroll to top
    
    * http
    
    * comment on pr
    
    * remove commenter
    
    * comment failing test
    
    * remove commented code

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+  setLogLevel,
+  initializeFirestore,
+} from "firebase/firestore"
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBvk41kRWgCj9cQ2Bn_WRcxuULTcV3TwqI",
@@ -10,11 +15,20 @@ export const firebaseConfig = {
   messagingSenderId: "479379145905",
   appId: "1:479379145905:web:8ceabb2b56f2d542190e90",
   measurementId: "G-JWJXD9J650",
+  // experimentalForceLongPolling: true,
+  experimentalAutoDetectLongPolling: true,
+  timeout: 5000,
 }
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
+const initialdb = initializeFirestore(app, {
+  experimentalAutoDetectLongPolling: true,
+})
+if (window.location.hostname === "localhost") {
+  connectFirestoreEmulator(initialdb, "localhost", 8082)
+  setLogLevel("debug")
+}
 export const db = getFirestore()
+
 export const analytics = getAnalytics(app)
-// import { setLogLevel } from "firebase/firestore"
-// setLogLevel("debug")
