357da4b31aeea57791fbdb471a405a828aa829ac
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-05-18 22:39:13 +0100

    tidy

--- cypress/integration/podcasts.spec.js
+++ cypress/integration/podcasts.spec.js
@@ -2,61 +2,110 @@
 
 const homeurl = "http://localhost:3000"
 
-describe("newsdesk login", () => {
+const switchPodCast = (podcast) => {
+  cy.get("#headlessui-menu-button-1").click()
+  cy.contains(podcast).click()
+}
+
+describe("newsdesk logged in", () => {
   beforeEach(() => {
+    cy.request(
+      "DELETE",
+      `http://localhost:${firebaseConfig.emulators.firestore.port}/emulator/v1/projects/${firebaseAppConfig.projectId}/databases/(default)/documents`,
+    )
     cy.visit(homeurl)
-  })
-
-  it("should have login button", function () {
-    cy.get("button").should("have.text", "Login with Google")
-    cy.contains("Login with Google").click()
-  })
-
-  it("should login", function () {
     cy.contains("Login with Google").click()
     cy.contains("Peach Otter").click()
     cy.get("h2").should("have.text", "⬆️ select a podcast ⬆️")
-  })
-})
-
-describe("newsdesk logged in", () => {
-  before(() => {
-    cy.visit(homeurl)
-    cy.contains("Login with Google").click()
-    cy.contains("Peach Otter").click()
-  })
-  beforeEach(() => {
-    // cy.visit(homeurl)
+    switchPodCast("dev sandbox")
   })
 
-  it("select podcast", function () {
-    cy.get("#headlessui-menu-button-1").click()
-    cy.contains("dev sandbox").click()
+  it("should be able to logout", () => {
+    cy.get("#logout").click()
+    cy.contains("Login with Google")
+  })
+
+  it("should be toggle between dark/light", () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000)
+    cy.get("html").then(($btn) => {
+      cy.get("#darklight").click()
+      if ($btn.hasClass("dark")) cy.get("html").should("have.not.class", "dark")
+      else cy.get("html").should("have.class", "dark")
+    })
+  })
+
+  it("should maximise the inbox", () => {
+    cy.get("#inbox-column").should("not.have.class", "col-span-3")
+    cy.get("button[title='toggle inbox expansion'").click()
+    cy.get("#inbox-column").should("have.class", "col-span-3")
+  })
+
+  it("should maximise the script", () => {
+    cy.get("#script-column").should("not.have.class", "col-span-3")
+    cy.get("button[title='toggle script expansion'").click()
+    cy.get("#script-column").should("have.class", "col-span-3")
+  })
+
+  it("should minimise the draft", () => {
+    cy.get("#draft-column").should("have.class", "col-span-3")
+    cy.get("button[title='toggle draft expansion'").click()
+    cy.get("#draft-column").should("not.have.class", "col-span-3")
+  })
+
+  it("should select podcast", () => {
     cy.url().should("include", "/#/dev")
   })
 
   it("should be able to create and delete a new item", () => {
-    cy.get("#headlessui-menu-button-1").click()
     cy.contains("dev sandbox").click()
 
-    cy.get("section[slotno=7] textarea").type("new item{enter}")
+    cy.get("section[slotno=7] textarea").type("new item{enter}", {
+      force: true,
+    })
     cy.get("section[slotno=7]").should("contain", "new item")
     cy.get("section[slotno=7] button[title='Delete']").click()
     cy.get("section[slotno=7]").should("not.contain", "new item")
   })
 
   it("should be able to create and share a new item", () => {
-    cy.get("#headlessui-menu-button-1").click()
-    cy.contains("dev sandbox").click()
-
     cy.get("section[slotno=7] textarea").type("new share item{enter}")
     cy.get("section[slotno=7]").should("contain", "new share item")
     cy.get("section[slotno=7] button[title='Share to dev2']").click()
     cy.get("section[slotno=7] button[title='Delete']").click()
     cy.get("section[slotno=7]").should("not.contain", "new share item")
-    cy.get("#headlessui-menu-button-1").click()
-    cy.contains("dev 2 sandbox").click()
-    // cy.scrollTo("top")
-    // cy.get("#inbox-column").should("contain", "new share item")
+    switchPodCast("dev 2 sandbox")
+    cy.get("#inbox-column").should("contain", "new share item")
   })
+
+  it("should create and delete items", () => {
+    switchPodCast("dev sandbox")
+    for (let section = 1; section <= 7; section++) {
+      for (let i = 1; i <= 3; i++)
+        cy.get(`section[slotno=${section}] textarea`).type(
+          `new item ${section} number ${i} {enter}`,
+          { force: true },
+        )
+    }
+    // @TODO: connect to firebase to check it happened
+    // @TODO: cleanup
+  })
+
+  it.skip("should be able to set slot titles", () => {
+    // @TODO: fix this test
+    switchPodCast("dev sandbox")
+    for (let section = 1; section <= 7; section++)
+      cy.get(`section[slotno=${section}] input`).type(
+        `lot ${section} title {enter}`,
+      )
+  })
+
+  it.skip("should move item between slots when dragging")
+  it.skip("should move item within a slot when dragging")
+  it.skip(
+    "should add item to slot and remove from inbox when dragging from inbox",
+  )
 })
+
+import firebaseConfig from "../../firebase.json"
+import { firebaseConfig as firebaseAppConfig } from "../../src/plugins/firebase"
