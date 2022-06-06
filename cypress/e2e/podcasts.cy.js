19226349d0eb10b647074091f359413a6a96016d
bhavinsen <senbhavin@gmail.com>
2022-06-06 11:35:50 +0530

    update cypress dependencies

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
 const homeurl = "http://localhost:3000"
--- src/store/item.ts
+++ src/store/item.ts
@@ -2,14 +2,7 @@ import { nanoid } from "nanoid"
 import { defineStore } from "pinia"
 import { Item } from "@/types/item"
 
-import {
-  collection,
-  doc,
-  setDoc,
-  onSnapshot,
-  updateDoc,
-  arrayRemove,
-} from "firebase/firestore"
+import { collection, doc, setDoc, onSnapshot } from "firebase/firestore"
 
 import { db } from "@/plugins/firebase"
 
@@ -37,10 +30,13 @@ export const useItemStore = defineStore("item", {
     },
 
     async removeItem(item: Item, podcastname: string, docname: string) {
-      const docRef = doc(collection(db, podcastname), docname)
-      await updateDoc(docRef, {
-        items: arrayRemove(item),
-      })
+      const index = this.itemList.findIndex((x) => x.id === item.id)
+
+      if (index < 0) throw new Error(`Can't find item [${item.id}]`)
+
+      this.itemList.splice(index, 1)
+      //TODO: remove only the individual item
+      return this.saveData(podcastname, docname)
     },
 
     async updateItem(item: Item, podcastname: string, docname: string) {
