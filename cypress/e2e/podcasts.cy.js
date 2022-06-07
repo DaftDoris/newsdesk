d92b1a72012a40c9841317d9933f725be01a25ff
bhavinsen <senbhavin@gmail.com>
2022-06-07 14:23:39 +0530

    updated test case

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -184,6 +184,9 @@ describe("newsdesk logged in", () => {
       .invoke("attr", "href")
       .should("eq", link)
     cy.get("section[slotno=7] ul li div a").click()
+    cy.get("section[slotno=7] ul li div a")
+      .invoke("attr", "href")
+      .should("eq", link)
     cy.get("section[slotno=7] button[title='Delete']").click()
     cy.get("section[slotno=7]").should(
       "not.contain",
