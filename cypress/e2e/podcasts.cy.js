5a9033712127f00ada8a746c4352b7f00a2fbd49
bhavinsen <senbhavin@gmail.com>
2022-06-07 18:47:33 +0530

    update links  test case

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -183,10 +183,11 @@ describe("newsdesk logged in", () => {
     cy.get("section[slotno=7] ul li div a")
       .invoke("attr", "href")
       .should("eq", link)
+    cy.get("section[slotno=7]").should(
+      "contain",
+      "https://twitter.com/PoliticusSarah/status/15207595...",
+    )
     cy.get("section[slotno=7] ul li div a").click()
-    cy.get("section[slotno=7] ul li div a")
-      .invoke("attr", "href")
-      .should("eq", link)
     cy.get("section[slotno=7] button[title='Delete']").click()
     cy.get("section[slotno=7]").should(
       "not.contain",
