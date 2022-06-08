c313b95f36d8eed3ffd031bc21d3df7551c7c646
bhavinsen <senbhavin@gmail.com>
2022-06-08 21:40:09 +0530

    Create 7 sections in script (#307)
    * Create 7 sections in script
    
    * remove content in script session and add test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <main
     v-show="initiated && isAuthenticated"
-    class="h-full grid grid-cols-5 gap-4 divide-x"
+    class="h-full grid grid-cols-5 gap-4 divide-x relative"
   >
+    <div class="absolute block left-0 bg-zinc-900/[.7] h-screen w-2/5 overlay-background" :class="{'hidden':!hideShowColumn.script}"></div>
     <div class="px-4 column-h overflow-y-auto" id="inbox-column" :class="{'col-span-3':hideShowColumn.inbox}" >
       <div class="flex justify-between items-center">
         <h2 class="text-2xl dark:text-white">Inbox</h2>
@@ -96,7 +97,17 @@
           />
       </ListActionButton>
       </div>
-      <p>coming soon...</p>
+      <div class="mt-20" id="script-data">
+        <div
+        v-for="slot in Array.from({ length: 7 }, (_, i) => 7 - i)"
+        :key="slot"
+        >
+          <Scripts
+            class="my-5"
+            :slotno="slot"
+          />
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -117,6 +128,7 @@ import InputCard from "@/components/molecules/Cards/InputCard.vue"
 import SlotTitleInput from "@/components/atoms/SlotTitleInput.vue"
 import ListActionButton from "@/components/atoms/ListActionButton.vue"
 import { PlusIcon, MinusIcon, ClipboardCopyIcon } from "@heroicons/vue/outline"
+import Scripts from "@/components/Script.vue"
 
 const authStore = useAuthStore()
 const itemStore = useItemStore()
@@ -258,5 +270,13 @@ h2 {
 }
 .column-h {
   height: calc(100vh - 72px);
+}
+.overlay-background{
+  top: -65px;
+}
+@media (max-width: 639px) {
+  .overlay-background {
+    top: 0px
+  }
 }
 </style>
