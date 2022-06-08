2fd3480922c669970ecb532c41296578735c0975
bhavinsen <senbhavin@gmail.com>
2022-06-08 12:18:57 +0530

    update links test case

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -187,17 +187,15 @@ describe("newsdesk logged in", () => {
       "contain",
       "https://twitter.com/PoliticusSarah/status/15207595...",
     )
-    cy.get("section[slotno=7] ul li div a").click()
-    if (cy.get("section[slotno=7] button[title='Delete']")) {
-      cy.get("section[slotno=7] button[title='Delete']").click({
-        multiple: true,
-        force: true,
-      })
-      cy.get("section[slotno=7]").should(
-        "not.contain",
-        "https://twitter.com/PoliticusSarah/status/15207595...",
-      )
-    }
+    window.open(link)
+    cy.get("section[slotno=7] button[title='Delete']").click({
+      multiple: true,
+      force: true,
+    })
+    cy.get("section[slotno=7]").should(
+      "not.contain",
+      "https://twitter.com/PoliticusSarah/status/15207595...",
+    )
   })
 })
 
