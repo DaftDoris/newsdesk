482d38941771c4aa8db76fdc45ada0ab7bc70189
himadri-brainium <100533170+himadri-brainium@users.noreply.github.com>
2022-06-18 04:28:17 +0530

    PODCAST PAGE - Display podcast title underneath thumbnail also solved

--- src/store/podcasts.ts
+++ src/store/podcasts.ts
@@ -12,33 +12,35 @@ interface State {
 export const usePodcastStore = defineStore("podcasts", {
   state: (): State => ({
     podcasts: [
-      { id: "smart7", name: "The Smart 7: UK", image: "/the-smart-7.jpg" },
+      { id: "smart7", name: "The Smart 7 UK", image: "/the-smart-7.jpg" },
       {
         id: "smart7ireland",
-        name: "The Smart 7: Ireland",
+        name: "The Smart 7 Ireland",
         image: "/smart-7-ireland.jpg",
       },
       {
         id: "smart7germany",
-        name: "The Smart 7: Germany",
+        name: "The Smart 7 Germany",
         image: "/smart-7-germany.jpg",
       },
-      { id: "heart7", name: "The Heat 7", image: "/heart-7.png" },
-      { id: "tsn7us", name: "TSN7: US", image: "/tsn-7.jpg" },
-      { id: "tsn7uk", name: "TSN7: UK", image: "/tsn-7-uk.jpg" },
-      { id: "tsn7aus", name: "TSN7: AUS", image: "/default-podcast-image.jpg" },
       { id: "blipblip", name: "Blip Blip!", image: "/blip-blip.jpg" },
-      { id: "dev", name: "dev sandbox", image: "/default-podcast-image.jpg" },
-      {
-        id: "dev2",
-        name: "dev 2 sandbox",
-        image: "/default-podcast-image.jpg",
-      },
-      {
-        id: "dev3",
-        name: "dev 3 sandbox",
-        image: "/default-podcast-image.jpg",
-      },
+
+      { id: "tsn7us", name: "TSN7 US", image: "/tsn-7.jpg" },
+      { id: "tsn7uk", name: "TSN7 UK", image: "/tsn-7-uk.jpg" },
+      { id: "heart7", name: "The Heat 7", image: "/heart-7.png" },
+
+      // { id: "tsn7aus", name: "TSN7 AUS", image: "/default-podcast-image.jpg" },
+      // { id: "dev", name: "dev sandbox", image: "/default-podcast-image.jpg" },
+      // {
+      //   id: "dev2",
+      //   name: "dev 2 sandbox",
+      //   image: "/default-podcast-image.jpg",
+      // },
+      // {
+      //   id: "dev3",
+      //   name: "dev 3 sandbox",
+      //   image: "/default-podcast-image.jpg",
+      // },
     ],
     readAccessPodcasts: [],
   }),
