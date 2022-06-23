ca8b32ae0ffcbeac30d19225652af4764c2255da
m_samyotech <m@samyotech.com>
2022-06-23 22:18:49 +0530

    clip section added

--- src/components/Script.vue
+++ src/components/Script.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="border-2 rounded-lg p-4">
-    <label class="w-11/12">
+  <div class="border-2 rounded-lg border-gray-400" @click="printMousePos()">
+    <label class="w-11/12 p-4">
       {{ slotno }} :
       <span class="text-gray-400">{{ slotno }} title</span>
     </label>
+     <Input
+      v-model="text"
+      :placeholder="`Enter things into ${slotno}...`"
+      @keydown.enter.exact.prevent="save"
+    />
+    <div class="clip-field">
+      <div class="clip-section">
+        <label for="">CLIP URL:</label>
+        <input
+          class="input break-all bg-transparent text-blue-600"
+          placeholder="URL"
+          v-model="clip_url"
+        />
+      </div>
+      <div class="clip-section border-l-2 border-gray-400">
+        <label for="">In:</label>
+        <input
+          class="input break-all bg-transparent w-8/12 text-gray-600"
+          placeholder="In"
+          v-model="in_time"
+        />
+      </div>
+      <div class="clip-section border-l-2 border-gray-400">
+        <input
+          class="input break-all bg-transparent w-10/12 text-gray-600"
+          placeholder="message"
+          v-model="in_msg"
+        />
+      </div>
+      <div class="clip-section border-l-2 border-gray-400">
+        <label for="">Out:</label>
+        <input
+          class="input break-all bg-transparent w-8/12 text-gray-600"
+          placeholder="Out"
+          v-model="out_time"
+        />
+      </div>
+      <div class="clip-section border-l-2 border-gray-400">
+        <input
+          class="input break-all bg-transparent w-10/12 text-gray-600"
+          placeholder="message"
+          v-model="out_msg"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+
+import Input from '@/components/atoms/Input.vue'
+import { ref } from 'vue'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps({
   slotno: {
@@ -15,6 +63,23 @@ const props = defineProps({
     default: 1,
   },
 })
+const clip_url = "bitly"
+const in_time = "00:00"
+const in_msg = '"Hello...'
+const out_time = "00:30"
+const out_msg = '...Goodbye"'
+const printMousePos = (event: { clientX: string; clientY: string })  =>  {
+   console.log("clientX: " + event.clientX +
+    " - clientY: " + event.clientY)
+}
+const emits = defineEmits(['save'])
+
+const text = ref<string>('')
+
+const save = () => {
+  emits('save', text.value, props.slotno)
+  text.value = ''
+}
 </script>
 
 <style scoped lang="scss">
@@ -24,5 +89,19 @@ const props = defineProps({
 }
 label {
   @apply ss-furniture text-5xl dark:text-white;
+}
+.clip-field {
+  @apply border-2 rounded-b-lg flex border-gray-400;
+  background-color: #e3e4e4;
+  margin: 20px -2px -2px -2px;
+}
+.clip-field label {
+  @apply text-base whitespace-nowrap mr-2;
+}
+.clip-field .clip-section {
+  @apply p-2 flex;
+}
+.clip-field .clip-section input {
+  @apply font-semibold outline-none;
 }
 </style>
--- src/components/atoms/Input.vue
+++ src/components/atoms/Input.vue
@@ -34,6 +34,6 @@ const value = useVModel(props, 'modelValue', emit)
   @apply shadow appearance-none border rounded w-full py-4 px-4 text-2xl font-bold leading-6 text-gray-700 dark:text-gray-50 dark:bg-gray-900 leading-tight focus:outline-none transition-colors;
 }
 .input::placeholder {
-  @apply ss-furniture;
+  
 }
 </style>
