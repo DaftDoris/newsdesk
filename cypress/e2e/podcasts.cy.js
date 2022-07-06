ae500bcfe340f873b667261bebefdeba0db6c866
reactjs <reactjs.shantinfosoft@gmail.com>
2022-07-06 23:43:46 +0530

    stable

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -187,7 +187,7 @@ describe("newsdesk logged in", () => {
     cy.get("section[slotno=7]").should("contain", "Remove item in inbox")
     cy.get("section[slotno=7] button[title='Share to podcast']").eq(0).click()
     cy.get("section[slotno=7] input[id='dev2'][type='checkbox']").click()
-    switchPodCast("dev 2 sandbox")
+    switchPodCast("dev sandbox")
     cy.get("#inbox-column").should("contain", "Remove item in inbox")
     cy.get("#inbox-column ul li listactionbutton[title='Delete']").eq(0).click({
       force: true,
