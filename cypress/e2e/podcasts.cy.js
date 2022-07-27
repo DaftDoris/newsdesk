4c8ed3ef5d055987e5fd570e083cad3256ccb0a1
reactjs <reactjs.shantinfosoft@gmail.com>
2022-07-27 14:03:54 +0530

    test worked

--- cypress/e2e/podcasts.cy.js
+++ cypress/e2e/podcasts.cy.js
@@ -251,20 +251,32 @@ describe("newsdesk logged in", () => {
 
   it("should be able to enter in script clips", () => {
     switchPodCast("dev sandbox")
+    cy.get("#script-column").should("not.have.class", "col-span-3")
+    cy.get("button[title='toggle script expansion'").click()
+    cy.get("#script-column").should("have.class", "col-span-3")
     cy.get("section[slotno=7] textarea").type("new share item{enter}", { force: true, })
     cy.get("section[slotno=7]").should("contain", "new share item")
-    // cy.get("#script-column button").click()
-
     cy.get("#script-data").then((el) => {
-      cy.get("#draft-column ul li p:eq(0)").trigger(
+      cy.get("section[slotno=7] ul div").eq(0).trigger(
         "dragend",
         {
-          clientX: el[0].getBoundingClientRect().right,
+          clientX: el[0].getBoundingClientRect().left,
           clientY: el[0].getBoundingClientRect().top,
         },
         { force: true },
       )
     })
+
+    // cy.get("#script-data").then((el) => {
+    //   cy.get("#draft-column ul li p:eq(0)").trigger(
+    //     "dragend",
+    //     {
+    //       clientX: el[0].getBoundingClientRect().right,
+    //       clientY: el[0].getBoundingClientRect().top,
+    //     },
+    //     { force: true },
+    //   )
+    // })
     cy.get("#clip_url").type("https://www.lipsum.com/", {
       force: true,
     })
@@ -287,10 +299,7 @@ describe("newsdesk logged in", () => {
     cy.get("#in_msg").should("contain", `hi`)
     cy.get("#out_msg").should("contain", `bye`)
   })
-  it.skip(
-    "should be able to enter in script clips",
-  )
-})
+  })
 
 import firebaseConfig from "../../firebase.json"
 import { firebaseConfig as firebaseAppConfig } from "../../src/plugins/firebase"
