1c04965e3c6fdb162823c6f55ac8d8d1c3aab490
ArindamReact <reactjs.shantinfosoft@gmail.com>
2022-09-02 14:57:06 +0530

    BUG: Deleting one clip field deletes all of them

--- src/store/item.ts
+++ src/store/item.ts
@@ -246,6 +246,19 @@ export const useItemStore = defineStore("item", {
       this.scriptItemList.splice(selectedIndex, 1)
       this.saveData(podcastname, docname)
     },
+    async deleteScriptClipFieldNew(id: string,ind:number, podcastname: string, docname: string) {
+        
+      this.scriptItemList[0].params
+        let selectedIndex = 0
+        this.scriptItemList.map((item, index) => {
+          if (item.id === id) {
+            selectedIndex = index
+          }
+        })
+        this.scriptItemList[selectedIndex].params.splice(ind,1);
+        this.saveData(podcastname, docname)
+          
+    },    
     connect(podcastname: string, docname: string) {
       onSnapshot(doc(db, podcastname, docname), (doc) => {
         this.slotTitleList = (
